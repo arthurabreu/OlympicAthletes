646d28b25133b27dea305cbcf49f1faf94f0ccf6
Arthur Abreu <arthurabreupro@gmail.com>
2023-07-07 10:45:54 -0300

    Initial commit

---
+++ app/src/main/java/com/arthurabreu/olympicathletes/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.arthurabreu.olympicathletes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.arthurabreu.olympicathletes.ui.theme.OlympicAthletesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OlympicAthletesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
